0c73a05718aadf83907af6fe55d456190d77437c
anil <anilkumar.kammalapalli@tarento.com>
2023-03-03 19:34:57 +0530

    added logger class

--- src/main/java/org/sunbird/workflow/service/impl/TaxonomyServiceImpl.java
+++ src/main/java/org/sunbird/workflow/service/impl/TaxonomyServiceImpl.java
@@ -1,8 +1,11 @@
 package org.sunbird.workflow.service.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.sunbird.workflow.consumer.ApplicationProcessingConsumer;
 import org.sunbird.workflow.core.WFLogger;
 import org.sunbird.workflow.models.WfRequest;
 import org.sunbird.workflow.postgres.repo.WfStatusRepo;
@@ -13,7 +16,7 @@ import java.util.Objects;
 @Service("taxonomyServiceImpl")
 public class TaxonomyServiceImpl implements WfServiceHandler {
 
-    private WFLogger logger = new WFLogger("TaxonomyServiceImpl");
+    Logger logger = LogManager.getLogger(TaxonomyServiceImpl.class);
 
     @Autowired
     private WfStatusRepo wfStatusRepo;
