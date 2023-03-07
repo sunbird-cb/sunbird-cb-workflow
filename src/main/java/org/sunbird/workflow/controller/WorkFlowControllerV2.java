3f7d993fc5b9424919757722e2ebf60abdf6bffc
Manas-tarento <manas.swain@tarento.com>
2023-03-07 10:40:48 +0530

    final changes

--- src/main/java/org/sunbird/workflow/controller/WorkFlowControllerV2.java
+++ src/main/java/org/sunbird/workflow/controller/WorkFlowControllerV2.java
@@ -14,26 +14,24 @@ import org.sunbird.workflow.service.WorkflowServiceV2;
 @RequestMapping("/v2/workflow")
 public class WorkFlowControllerV2 {
 
-	@Autowired
-	private WorkflowServiceV2 workflowService;
+    @Autowired
+    private WorkflowServiceV2 workflowService;
 
-	@PostMapping("/taxonomy/transition")
-	public ResponseEntity<Response> wfTransition(@RequestHeader String userToken, @RequestBody WfRequest wfRequest) {
-		Response response = workflowService.workflowTransition(userToken, wfRequest);
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
+    @PostMapping("/taxonomy/transition")
+    public ResponseEntity<Response> wfTransition(@RequestHeader String userToken, @RequestBody WfRequest wfRequest) {
+        Response response = workflowService.workflowTransition(userToken, wfRequest);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
-	@GetMapping(path = "taxonomy/{wfId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Response> getWfApplication(@RequestHeader String userToken,
-													 @PathVariable("wfId") String wfId) {
-		Response response = workflowService.getWfApplication(userToken, wfId);
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
+    @GetMapping(path = "taxonomy/{wfId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response> getWfApplication(@RequestHeader String userToken, @PathVariable("wfId") String wfId) {
+        Response response = workflowService.getWfApplication(userToken, wfId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
-	@PostMapping(path = "taxonomy/search", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Response> wfApplicationSearch(@RequestHeader String userToken,
-														@RequestBody SearchCriteria searchCriteria) {
-		Response response = workflowService.wfApplicationSearch(userToken, searchCriteria);
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
+    @PostMapping(path = "taxonomy/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response> wfApplicationSearch(@RequestHeader String userToken, @RequestBody SearchCriteria searchCriteria) {
+        Response response = workflowService.wfApplicationSearch(userToken, searchCriteria);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
