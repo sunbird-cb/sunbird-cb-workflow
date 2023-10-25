4243fd4fd385718d86adc3e61cfd27b969396025
sahilchaudhary <sahil.chaudhary@tarento.com>
2023-10-25 17:09:41 +0530

    Made the code review changes

--- src/main/java/org/sunbird/workflow/service/impl/DomainWhiteListWorkFlowServiceImpl.java
+++ src/main/java/org/sunbird/workflow/service/impl/DomainWhiteListWorkFlowServiceImpl.java
@@ -23,10 +23,7 @@ import org.sunbird.workflow.service.UserProfileWfService;
 import org.sunbird.workflow.service.Workflowservice;
 import org.sunbird.workflow.utils.CassandraOperation;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -53,16 +50,31 @@ public class DomainWhiteListWorkFlowServiceImpl implements DomainWhiteListWorkFl
     @Override
     public Response createDomainWorkFlow(String rootOrg, String org, WfRequest wfRequest) {
         Response response = new Response();
-        HashMap<String, Object> updatedFieldValue = wfRequest.getUpdateFieldValues().stream().findFirst().get();
-        HashMap<String, String> toValue = (HashMap<String, String>) updatedFieldValue.get("toValue");
-        String domainValue =  toValue.get("domain").isEmpty() ? "" : toValue.get("domain");
-        List<WfDomainLookup> domainLookup = wfDomainLookupRepo.findByDomainName(domainValue);
-        if(CollectionUtils.isNotEmpty(domainLookup)) {
-            response.put(Constants.MESSAGE, Constants.DOMAIN_NAME_REQUEST_ALREADY_ERROR_MSG + ": " + domainValue);
-            response.put(Constants.STATUS, HttpStatus.ACCEPTED);
-            return response;
+        try {
+            HashMap<String, Object> updatedFieldValue = wfRequest.getUpdateFieldValues().stream().findFirst().get();
+            HashMap<String, String> toValue = (HashMap<String, String>) updatedFieldValue.get("toValue");
+            String domainValue = toValue.get("domain").isEmpty() ? "" : toValue.get("domain");
+            List<WfDomainLookup> domainLookup = wfDomainLookupRepo.findByDomainName(domainValue);
+            if (CollectionUtils.isNotEmpty(domainLookup)) {
+                response.put(Constants.MESSAGE, Constants.DOMAIN_NAME_REQUEST_EXIST_MSG + ": " + domainValue);
+                response.put(Constants.STATUS, HttpStatus.ACCEPTED);
+                return response;
+            }
+            response = workflowService.workflowTransition(rootOrg, org, wfRequest);
+            if (HttpStatus.OK.compareTo((HttpStatus) response.getResult().get(Constants.STATUS)) == 0) {
+                WfDomainLookup wfDomainLookup = new WfDomainLookup();
+                Map<String, Object> dataObject = (Map<String, Object>) response.getResult().getOrDefault(Constants.DATA, new HashMap());
+                List<String> wfIdList = (List<String>) dataObject.getOrDefault(Constants.WF_IDS_CONSTANT, new ArrayList());
+                wfDomainLookup.setWfId(wfIdList.get(0));
+                wfDomainLookup.setDomainName(domainValue);
+                wfDomainLookupRepo.save(wfDomainLookup);
+            }
+        } catch (Exception e) {
+            String errMsg = String.format("Failed to get the stats for course. Exception: ", e.getMessage());
+            response.put(Constants.ERROR_MESSAGE, errMsg);
+            response.put(Constants.STATUS, HttpStatus.INTERNAL_SERVER_ERROR);
+            response.setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        response = workflowService.workflowTransition(rootOrg, org, wfRequest);
         return response;
     }
 
