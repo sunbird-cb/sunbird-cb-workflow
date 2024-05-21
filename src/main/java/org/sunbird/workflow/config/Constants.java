20ecd7d9869a69c8898e22aa18141fc40fb596d9
sahilchaudhary <sahil.chaudhary@tarento.com>
2024-05-21 15:28:47 +0530

    Updating the profile for bulk approval

--- src/main/java/org/sunbird/workflow/config/Constants.java
+++ src/main/java/org/sunbird/workflow/config/Constants.java
@@ -325,5 +325,8 @@ public class Constants {
 	public static final String EXTERNAL_SYSTEM_ID = "externalSystemId";
 	public static final String EXTERNAL_SYSTEM = "externalSystem";
 	public static final String IS_ORG_TRANSFER_REQUEST = "isOrgTransferRequest";
+	public static final String PROFILE_STATUS = "profileStatus";
+	public static final String NOT_VERIFIED = "NOT-VERIFIED";
+	public static final String VERIFIED = "VERIFIED";
 
 }
