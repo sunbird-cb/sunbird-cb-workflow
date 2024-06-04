c210c60e13d173163f5b24f6d4511f1c2b3463f5
ravisaurav-tarento <142487505+ravisaurav-tarento@users.noreply.github.com>
2024-06-04 10:18:31 +0530

    Bug fixes for group language anddesignation validation (#192)
    * Bug fixes for group language anddesignation validation
    
    * improved save and fetch from redis
    
    * modified property reading config file
    
    * changes wrt redis red and write
    
    * modified language error message

--- src/main/java/org/sunbird/workflow/config/Constants.java
+++ src/main/java/org/sunbird/workflow/config/Constants.java
@@ -331,5 +331,6 @@ public class Constants {
 	public static final String CURRENT_STATUS_KEY = "currentStatus";
 	public static final String PROFILE_STATUS_UPDATED_ON = "profileStatusUpdatedOn";
 	public static final String PROFILE_STATUS_UPDATED_MSG_VIEWED = "isProfileUpdatedMsgViewed";
+	public static final String REDIS_COMMON_KEY = "workflow_";
 
 }
